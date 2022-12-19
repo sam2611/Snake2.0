4bcc39ad0bb771600f9a51be9502a28e036182b1
Sakshi Mishra <84084388+sam2611@users.noreply.github.com>
2022-12-19 20:34:13 +0530

    Add files via upload

---
+++ snakegame/src/snakegame/Main.java
@@ -0,0 +1,25 @@
+package snakegame;
+import java.awt.Color;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JFrame f= new JFrame();
+		
+		f.setTitle("Snake Game");
+		f.setBounds(10, 10, 905, 700);
+		f.setResizable(false);
+//		f.setVisible(true);
+		
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setBackground(Color.DARK_GRAY);
+		
+		Gameplay gameplay= new Gameplay();
+		f.add(gameplay);
+		f.setVisible(true);
+	}
+
+}
